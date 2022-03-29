9ea4c3d8aa615c64773290f2d144ea512d40aabe
Everson Mariano <mariano.computacao@gmail.com>
2022-03-29 12:32:06 -0300

    Conecting Angular with add employee REST API

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-frontend';
+  title = 'Angular and Spring Boot CRUD Full Stack App';
+
 }
--- angular-frontend/src/app/employee.service.ts
+++ angular-frontend/src/app/employee.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Employee } from './employee';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @Injectable({
   providedIn: 'root'
@@ -15,4 +16,11 @@ export class EmployeeService {
   getEmployeesList(): Observable<Employee[]> {
     return this.httpClient.get<Employee[]>(`${this.baseURL}`);
   }
+
+  createEmployee(employee: Employee): Observable<Object>{
+    return this.httpClient.post(`${this.baseURL}`, employee);
+  }
+
+
+
 }

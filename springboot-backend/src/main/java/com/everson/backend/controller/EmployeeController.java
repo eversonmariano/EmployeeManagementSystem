c1e15cc017bf908aa4804961e951e054dcbe1218
Everson Mariano <mariano.computacao@gmail.com>
2022-03-28 23:44:06 -0300

    Method POST implemented

--- springboot-backend/src/main/java/com/everson/backend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/everson/backend/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.everson.backend.controller;
 import com.everson.backend.model.Employee;
 import com.everson.backend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,13 @@ public class EmployeeController {
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
     }
+
+    //Create employee REST API
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+
 
 
 

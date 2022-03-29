af8a31e44316b06da502cf87e140cad496edcf9c
Everson Mariano <mariano.computacao@gmail.com>
2022-03-29 15:57:51 -0300

    Creating GetByID and UPDATE employee - REST API

--- springboot-backend/src/main/java/com/everson/backend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/everson/backend/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.everson.backend.controller;
 
+import com.everson.backend.exception.ResourceNotFoundException;
 import com.everson.backend.model.Employee;
 import com.everson.backend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,20 +17,40 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    //get all employees
+    //Get All employees
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
     }
 
-    //Create employee REST API
+    //Create employee - REST API
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
     }
 
 
+    //GET employee By Id - REST API
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id).
+                orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+        return ResponseEntity.ok(employee);
+    }
 
+    //UPDATE employee - REST API
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,@RequestBody Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id).
+                orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
 
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
 
+        Employee updateEmployee = employeeRepository.save(employee);
+
+        return ResponseEntity.ok(updateEmployee);
+
+    }
 }
